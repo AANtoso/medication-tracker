cd4ad3f61d6fb4c7ba48b71be0d2a559328a4a6d
github username <github email address>
2019-12-05 18:08:22 -0500

    Created model associations.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,5 @@
 class Patient < ApplicationRecord
+    belongs_to :provider
+    has_many :prescriptions
+    has_many :medications, through: :prescriptions
 end
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,2 +1,6 @@
 class Provider < ApplicationRecord
+
+    has_many :prescriptions
+    has_many :medications, through: :prescriptions
+
 end
