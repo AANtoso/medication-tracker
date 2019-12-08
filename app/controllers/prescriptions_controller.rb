2a975eb19116adf201f78463f7bdac6c5fe036a2
github username <github email address>
2019-12-08 12:07:27 -0500

    Added CRUD methods.

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -6,26 +6,45 @@ class PrescriptionsController < ApplicationController
     end
 
     def new
-        
+        if params[:provider_id] && @provider = Provider.find_by_id(params[:provider_id])
+            @prescription = @provider.prescriptions.build
+        else 
+            @prescription = Prescription.new
+        end
     end
 
     def create
+        @prescription = current_patient.prescriptions.build(prescription_params)
+        if @prescription.save
+            redirect_to presctiption_path(@prescription)
+        else
+            render 'new'
+        end
     end
 
     def show
+        @prescription = Prescription.find(params[:id])
     end
 
     def edit
+        @prescription = Prescription.find(params[:id])
     end
 
     def update
+        @prescription = Prescription.find(params[:id])
+        @prescription.update(prescription_params)
+        redirect_to prescription_path(@prescription)
     end
 
     def destroy
+        @prescription = Prescription.find(params[:id])
+        @prescription.destroy
+        redirect_to prescription_path
     end
 
     private
 
     def presctiption_params
+        params.require(:prescription).permit(:patient_id, :provider_id, :prescribed_date)
     end
 end
