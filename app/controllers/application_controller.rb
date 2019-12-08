08c129a127df4e0381308930071b0887485c455c
github username <github email address>
2019-12-08 13:11:10 -0500

    Created CRUD methods.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_patient, :logged_in?, :authenticate_patient
+
+    private
+
+    def current_patient
+        @current_patient ||= Patient.find_by(id: session[:patient_id]) if session[:patient_id]
+    end
+
+    def logged_in
+        !!current_patient
+    end
+
+    def authenticate_patient
+        if !logged_in?
+            redirect_to root_path
+        end
+    end
 end
