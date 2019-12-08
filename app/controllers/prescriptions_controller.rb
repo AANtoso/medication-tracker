0862e051af7bc8e18befa5addf05ba517ef9ac3f
github username <github email address>
2019-12-08 11:50:04 -0500

    Created CRUD methods.

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -1,2 +1,31 @@
 class PrescriptionsController < ApplicationController
+    before_action :authenticate_patient
+    
+    def index
+        @prescriptions = Presctiption.all
+    end
+
+    def new
+        
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def presctiption_params
+    end
 end
