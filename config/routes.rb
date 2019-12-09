bd30113e3d51d9a15769bd3e2d9e2cbc09f5324c
github username <github email address>
2019-12-09 10:33:41 -0500

    Added routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+
+  root 'sessions#index'
+
+  get '/register' => 'patients#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   resources :prescriptions
-  get 'sessions/index'
-  get 'sessions/create'
-  get 'sessions/new'
-  get 'sessions/edit'
-  get 'sessions/show'
-  get 'sessions/update'
-  get 'sessions/destroy'
+  
   resources :providers
   resources :patients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
