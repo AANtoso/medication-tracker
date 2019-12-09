6160b7d1ab1e04121b95de5b5132ef646dd54773
github username <github email address>
2019-12-09 10:39:41 -0500

    Uncommented bcrypt gem, debuged patient/new.html.erb.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,7 +1,7 @@
 class Patient < ApplicationRecord
     has_secure_password
     has_many :prescriptions
-    has many :providers, through: :prescriptions
+    has_many :providers, through: :prescriptions
 
     validates :username, :email, presence: true
     validates :username, :email, uniqueness: true
