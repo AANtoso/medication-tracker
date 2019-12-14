3e24112c7e3648e7559854ce3adf5444c2557c20
github username <github email address>
2019-12-14 12:04:18 -0500

    Spacing.

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -3,7 +3,8 @@ class Prescription < ApplicationRecord
     belongs_to :patient
     belongs_to :provider
     accepts_nested_attributes_for :provider
-
+    
+    
     def provider_attributes=(provider)
         self.provider = Provider.find_or_create_by(name: provider[:name])
         self.provider.update(provider)
